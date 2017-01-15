79b032e4d866c605f411d086274245259b938d6c
Mitsuru Nakada <m-nakada@nekojarashi.com>
2017-01-15 21:19:06 +0900

    Run ‘go fmt’

--- darwin2macos_test.go
+++ darwin2macos_test.go
@@ -5,20 +5,20 @@ import (
 )
 
 func present(t *testing.T, darwin string, macos string) {
-    str, present := ToMacOS(darwin)
-    if present == false {
-        t.Error(darwin, "is not present.")
-    }
+	str, present := ToMacOS(darwin)
+	if present == false {
+		t.Error(darwin, "is not present.")
+	}
 	if str != macos {
 		t.Error(darwin, "is not equal to", macos)
 	}
 }
 
 func unknwon(t *testing.T, darwin string) {
-    str, present := ToMacOS(darwin)
-    if present == true {
-        t.Error(darwin, "is present.")
-    }
+	str, present := ToMacOS(darwin)
+	if present == true {
+		t.Error(darwin, "is present.")
+	}
 	if str != "Unknown" {
 		t.Error(darwin, "is not equal to \"Unknown\"")
 	}
@@ -26,12 +26,12 @@ func unknwon(t *testing.T, darwin string) {
 
 func TestToMacOS(t *testing.T) {
 	present(t, "16.3.0", "10.12.2")
-    present(t, "16.1.0", "10.12.1")
-    present(t, "16.0.0", "10.12.0")
+	present(t, "16.1.0", "10.12.1")
+	present(t, "16.0.0", "10.12.0")
 
-    unknwon(t, "0.0.0")
+	unknwon(t, "0.0.0")
 }
 
 func TestDebugDump(t *testing.T) {
-    DebugDumpVersions()
+	DebugDumpVersions()
 }
